39a4158796c83674848ed1a4663abe477d767d49
Catishere <viktor.gyoshev@gmail.com>
2018-06-17 07:25:26 +0300

    fixed identation, tab to spaces

--- js/clientsidelogic.js
+++ js/clientsidelogic.js
@@ -8,341 +8,335 @@ var currentRoom = null;
 initPage();
 
 $('#login').click(function() {
-	loginHandle(false);
+    loginHandle(false);
 });
 
 $('#register').click(function() {
-	loginHandle(true);
+    loginHandle(true);
 });
 
 $('#toggleChat').click(function() {
-	$("#chatlog").toggle();
+    $("#chatlog").toggle();
 });
 
 function loginHandle(register) {
-	var username = $('#username').val();
-	var password = $('#password').val();
-	
-	if (username != '' && password != '')
-	{
-		if (username.match(/^[0-9a-zA-Z]{1,16}$/))
-			socket.emit('attemptlogin', username, password, register);
-		else
-			displayError("Incorrect username!");
-	} else {
-		displayError("Enter username and password!");
-	}
+    var username = $('#username').val();
+    var password = $('#password').val();
+    
+    if (username != '' && password != '')
+    {
+        if (username.match(/^[0-9a-zA-Z]{1,16}$/))
+            socket.emit('attemptlogin', username, password, register);
+        else
+            displayError("Incorrect username!");
+    } else {
+        displayError("Enter username and password!");
+    }
 }
 
 $('#game-container').click(function() {
-	var offset = $('.game').offset();
-	var x = event.clientX - offset.left;
-	var y = event.clientY - offset.top;
+    var offset = $('.game').offset();
+    var x = event.clientX - offset.left;
+    var y = event.clientY - offset.top;
 
-	if (yourname != null && $('#'+yourname) != null) {
-		var ratio = (y * 1.0) / 768;
-		if (ratio < 0.95 && ratio > 0.3 && x + y != 0)
-			socket.emit('playermove', x, y, yourname, null);
-	}	 
+    if (yourname != null && $('#'+yourname) != null) {
+        var ratio = (y * 1.0) / 768;
+        if (ratio < 0.95 && ratio > 0.3 && x + y != 0)
+            socket.emit('playermove', x, y, yourname, null);
+    }     
 });
 
 function playAudio(audio) {
-	if (!mute)
-		audio.play();
+    if (!mute)
+        audio.play();
 }
 
 function displayError(message) {
-	var div = document.createElement("div");
-	div.innerHTML = message;
-	div.className = "error";
-	$(".game").append(div);
-	setTimeout(function(){ div.remove(); }, 6000);
+    var div = document.createElement("div");
+    div.innerHTML = message;
+    div.className = "error";
+    $(".game").append(div);
+    setTimeout(function(){ div.remove(); }, 6000);
 }
 
 function initPage() {
-	
-	messageSound = new Audio('sound/new_message_sound.mp3');
-	
-	$( "#muteButton" ).click( function() {
-		if (mute) {
-			$("#muteButton").attr('src', '/images/unmute.png');
-			mute = false;
-		}
-		else {
-			$("#muteButton").attr('src', '/images/mute.png');
-			mute = true;
-		}
-	});
+    
+    messageSound = new Audio('sound/new_message_sound.mp3');
+    
+    $( "#muteButton" ).click( function() {
+        if (mute) {
+            $("#muteButton").attr('src', '/images/unmute.png');
+            mute = false;
+        }
+        else {
+            $("#muteButton").attr('src', '/images/mute.png');
+            mute = true;
+        }
+    });
 }
 
 function spawnLocation(lastRoom) {
-	var spawn = {};
-	
-	switch(currentRoom) {
-		case "mars":
-		if (lastRoom == "earth") {
-			var pos = $('#teleport_right').position();
-			spawn.x = pos.left - 50;
-			spawn.y = pos.top + 100;
-		} else if (lastRoom == null) {
-			var pos = $("#teleport_left").position();
-			spawn.x = pos.left + 250; 
-			spawn.y = pos.top + 100;
-		}
-		break;
-		
-		case "earth":
-			if (lastRoom == "mars") {
-				var pos = $("#teleport_left").position();
-				spawn.x = pos.left + 250; 
-				spawn.y = pos.top + 100;
-			}
-		break;
-	}
-	return spawn;
+    var spawn = {};
+    
+    switch(currentRoom) {
+        case "mars":
+        if (lastRoom == "earth") {
+            var pos = $('#teleport_right').position();
+            spawn.x = pos.left - 50;
+            spawn.y = pos.top + 100;
+        } else if (lastRoom == null) {
+            var pos = $("#teleport_left").position();
+            spawn.x = pos.left + 250; 
+            spawn.y = pos.top + 100;
+        }
+        break;
+        
+        case "earth":
+            if (lastRoom == "mars") {
+                var pos = $("#teleport_left").position();
+                spawn.x = pos.left + 250; 
+                spawn.y = pos.top + 100;
+            }
+        break;
+    }
+    return spawn;
 }
 
 function prepareRoom(room) {
-	
-	var objectlen = 0;
+    
+    var objectlen = 0;
 
-	if (currentRoom == room) {
-		return true;
-	}
-	
-	if (currentRoom != null)
-		socket.emit('leaveroom', currentRoom, yourname);
-	
-	var lastRoom = currentRoom;
-	currentRoom = room;
-	
-	if (yourname != null) socket.emit('joinroom', room, yourname);
-	
+    if (currentRoom == room) {
+        return true;
+    }
+    
+    if (currentRoom != null)
+        socket.emit('leaveroom', currentRoom, yourname);
+    
+    var lastRoom = currentRoom;
+    currentRoom = room;
+    
+    if (yourname != null) socket.emit('joinroom', room, yourname);
+    
     $('.object').remove();
-	$('.player').remove();
-	
-	$('.game').css('background-image', 'url("/images/'+ room +'.jpg")');
-	
-	switch(room) { // needs separate object creation for easier object addition
+    $('.player').remove();
+    
+    $('.game').css('background-image', 'url("/images/'+ room +'.jpg")');
+    
+    switch(room) { // needs separate object creation for easier object addition
     case "mars":
-		createObject("teleport_left", "teleport.png", true, "0%", "40%");
+        createObject("teleport_left", "teleport.png", true, "0%", "40%");
         createObject("teleport_right", "teleport.png", true, "85%", "40%");
-		objectlen = 2;
+        objectlen = 2;
 
         break;
     case "earth":
-		createObject("teleport_left", "teleport.png", true, "0%", "40%");
+        createObject("teleport_left", "teleport.png", true, "0%", "40%");
         createObject("teleport_right", "teleport.png", true, "85%", "40%");
-		objectlen = 2;
+        objectlen = 2;
 
         break;
     default:
-		console.log("unknown room");
-	}
-	
-	do {
-		objects = $(".object");
-	} while (objects.length < objectlen);
-	
-	var spawn = spawnLocation(lastRoom);
-	socket.emit('reqspawn', ' ' + currentRoom, yourname, spawn.x , spawn.y);
+        console.log("unknown room");
+    }
+    
+    do {
+        objects = $(".object");
+    } while (objects.length < objectlen);
+    
+    var spawn = spawnLocation(lastRoom);
+    socket.emit('reqspawn', ' ' + currentRoom, yourname, spawn.x , spawn.y);
 }
 
 function createObject(name, decal, type, x, y) { /*x, y - string N% Npx etc.*/
-	var object = document.createElement("img");
-	object.id = name;
-	object.className = (type) ? "object object-below-player" : "object object-above-player";  //true - under, false - above;
-	object.src = "images/" + decal;
-	object.style.left = x;
-	object.style.top = y;
-	$(".game").append(object);
+    var object = document.createElement("img");
+    object.id = name;
+    object.className = (type) ? "object object-below-player" : "object object-above-player";  //true - under, false - above;
+    object.src = "images/" + decal;
+    object.style.left = x;
+    object.style.top = y;
+    $(".game").append(object);
 }
 
 function playerObjectCollision(x, y) {
-	var result = null;
-	objects.each(function(i) {
-		var pos = $(this).position();
-		var objX = Number(pos.left);
-		var objY = Number(pos.top);
-		var objEndX = objX + Number($(this).width());
-		var objEndY = objY + Number($(this).height());
-		if (x > objX && y > objY && x < objEndX  && y < objEndY) {
-			result = $(this).attr('id');
-			return false;
-		}
-	});
-	return result;
+    var result = null;
+    objects.each(function(i) {
+        var pos = $(this).position();
+        var objX = Number(pos.left);
+        var objY = Number(pos.top);
+        var objEndX = objX + Number($(this).width());
+        var objEndY = objY + Number($(this).height());
+        if (x > objX && y > objY && x < objEndX  && y < objEndY) {
+            result = $(this).attr('id');
+            return false;
+        }
+    });
+    return result;
 }
 
 function executeObjectAction(name, objectTrigger) {
-	
-	//var object = $('#' + objectTrigger);
-	//var objPos = object.position();
-	var isYou = (name == yourname);
-	switch (objectTrigger) {
-		case "teleport_right":
-			if (isYou) prepareRoom("earth");
-			break;
-		case "teleport_left":
-			if (isYou) prepareRoom("mars");
-			break;
-		default:
-	}
+    
+    //var object = $('#' + objectTrigger);
+    //var objPos = object.position();
+    var isYou = (name == yourname);
+    switch (objectTrigger) {
+        case "teleport_right":
+            if (isYou) prepareRoom("earth");
+            break;
+        case "teleport_left":
+            if (isYou) prepareRoom("mars");
+            break;
+        default:
+    }
 }
 
 function checkAction(name, x, y) {
-	executeObjectAction(name, playerObjectCollision(x, y));
+    executeObjectAction(name, playerObjectCollision(x, y));
 }
 
 function spawnPlayer(name, x, y) {
-	var fig = document.createElement("figure");
-	var player = document.createElement("img");
-	var nametext = document.createElement("figcaption");
-	var size = y * 0.266 - 46.402;
-	fig.id = name;
-	player.src = "images/cat2.png";
-	player.style.height = "60px";
-	player.style.width = "60px";
-	fig.className = "player";
-	nametext.innerHTML = name;
-	if (x != null ) {
-		fig.style.left = x + 'px';
-		fig.style.top = y + 'px';
-		player.style.height = size + 'px';
-		player.style.width = size + 'px';
-	}
-	fig.appendChild(player);
-	fig.appendChild(nametext);
-	$(".game").append(fig);
+    var fig = document.createElement("figure");
+    var player = document.createElement("img");
+    var nametext = document.createElement("figcaption");
+    var size = y * 0.266 - 46.402;
+    fig.id = name;
+    player.src = "images/cat2.png";
+    player.style.height = "60px";
+    player.style.width = "60px";
+    fig.className = "player";
+    nametext.innerHTML = name;
+    if (x != null ) {
+        fig.style.left = x + 'px';
+        fig.style.top = y + 'px';
+        player.style.height = size + 'px';
+        player.style.width = size + 'px';
+    }
+    fig.appendChild(player);
+    fig.appendChild(nametext);
+    $(".game").append(fig);
 }
 
 function displayChat(name, message) {
-	var div = document.createElement("div");
-	var player = $("#" + name);
-	div.innerHTML = message + "\n\n";
-	div.className = "chat-bubble tri-right btm-left-in";
-	if (player.find('.chat-bubble').length)
-		player.find('.chat-bubble').remove();
-	var marginres = 50 + 25 * (message.length/10);
-	div.style.marginTop = "-" + marginres + "px";
-	player.prepend(div);
-	if (name != yourname) playAudio(messageSound);
-	setTimeout(function(){ div.remove();}, 7950);
+    var div = document.createElement("div");
+    var player = $("#" + name);
+    div.innerHTML = message + "\n\n";
+    div.className = "chat-bubble tri-right btm-left-in";
+    if (player.find('.chat-bubble').length)
+        player.find('.chat-bubble').remove();
+    var marginres = 50 + 25 * (message.length/10);
+    div.style.marginTop = "-" + marginres + "px";
+    player.prepend(div);
+    if (name != yourname) playAudio(messageSound);
+    setTimeout(function(){ div.remove();}, 7950);
 }
 
 $('form').submit(function(){
-  if ($('#username').val() != '')
-  {
-
-  }
-  else 
-  {
-	if ($('#m').val().match(/^[-!@#$%^&*()_+|~={}\[\]:;<>?,.\/0-9a-zA-Z ]{1,80}$/))
-	{
-		socket.emit('chat message', yourname, $('#m').val(), currentRoom);
-		$('#m').focus();
-	}else 
-		displayError("Invalid Message!");
-	$('#m').val('');
-  }
-	  return false;
+    if ($('#username').val() == '') {
+        if ($('#m').val().match(/^[-!@#$%^&*()_+|~={}\[\]:;<>?,.\/0-9a-zA-Z ]{1,80}$/)) {
+            socket.emit('chat message', yourname, $('#m').val(), currentRoom);
+            $('#m').focus();
+        } else 
+            displayError("Invalid Message!");
+        
+        $('#m').val('');
+    }
+    return false;
 });
 
 socket.on('spawn', function(target, name, x, y){
-	if (yourname != null) {
-		if (yourname == target) {
-			spawnPlayer(name, x, y);
-		} else if (target == 'A L L') {
-			spawnPlayer(name, x, y);
-			if (name != yourname) {
-				var catplayer = $('#' + yourname);
-				socket.emit('reqspawn', name, yourname, catplayer.position().left, catplayer.position().top);
-			}
-		}
-	}
+    if (yourname != null) {
+        if (yourname == target) {
+            spawnPlayer(name, x, y);
+        } else if (target == 'A L L') {
+            spawnPlayer(name, x, y);
+            if (name != yourname) {
+                var catplayer = $('#' + yourname);
+                socket.emit('reqspawn', name, yourname, catplayer.position().left, catplayer.position().top);
+            }
+        }
+    }
 });
 
 socket.on('playerleftroom', function (name) {
-	$("#" + name).remove();
+    $("#" + name).remove();
 });
 
 socket.on('chat message', function(name, msg, control){
   
-  if (control == 'disconnect') {
-	  console.log("disappear please");
-	  $('#'+name).remove();
-  }
-  if (name == null || control != null)
-  {
-	$('#messages').append($('<li>').text(msg));
-  }
-  else
-  {
-	$('#messages').append($('<li>').text('' + name + ': ' + msg));
-	displayChat(name, msg);
-  }
-  var element = document.getElementById("messages");
-  element.scrollTop = element.scrollHeight;
+    if (control == 'disconnect') {
+        console.log("disappear please");
+        $('#'+name).remove();
+    }
+    if (name == null || control != null) {
+        $('#messages').append($('<li>').text(msg));
+    }
+    else {
+        $('#messages').append($('<li>').text('' + name + ': ' + msg));
+        displayChat(name, msg);
+    }
+    
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
 });
 
 socket.on('errors', function(message){
-	displayError(message);
+    displayError(message);
 });
 
 socket.on('login', function(){
-	$("#myModal").hide();
-	yourname = $('#username').val();
-	prepareRoom("mars");
-	$('#username').val('');
+    $("#myModal").hide();
+    yourname = $('#username').val();
+    prepareRoom("mars");
+    $('#username').val('');
 });
 
 socket.on('playermove', function(x, y, name, control){
-	if (name == null) {
-		console.log("Error, player is not found");
-	} else {
-		
+    if (name == null) {
+        console.log("Error, player is not found");
+    } else {
+        
 
-	var speed = 2.85;
-	var size = y * 0.238 - 41.32;
-	x = x - size/2;
-	y = y - size/2;
-	var catplayer = $("#" + name);
-	var img = catplayer.find('img');
+        var speed = 2.85;
+        var size = y * 0.238 - 41.32;
+        x = x - size/2;
+        y = y - size/2;
+        var catplayer = $("#" + name);
+        var img = catplayer.find('img');
 
-	catplayer.stop();
-	img.stop();
-	
-	var xpos = parseInt(catplayer.css("left"));
-	var ypos = parseInt(catplayer.css("top"));
-	var xMove = x - xpos;
-	var yMove = y - ypos;
-	var absXMove = Math.abs(xMove);
-	var absYMove = Math.abs(yMove);
-	var xyMove = Math.sqrt(absXMove*absXMove + absYMove*absYMove);
-	var flip = (xMove < 0) ? 1:-1;
-	img.css("transform", "scaleX("+ flip +")");
-	
-	if (xMove >= 0) xMove = "+=" + xMove;
-	else xMove = "-=" + -xMove;
-	
-	if (yMove >= 0) yMove = "+=" + yMove;
-	else yMove = "-=" + -yMove;
-	
-	var realSpeed = (control == "instant") ? 0 : Math.round(xyMove * speed);
-	
-	catplayer.animate({
-			left: xMove,
-			top: yMove
-		}, realSpeed, "linear", function() {
-			checkAction(name, x, y);
-	});
-	
-	img.animate({
-		height: size,
-		width: size
-	}, realSpeed, "linear");
-  }
-  var element = document.getElementById("messages");
-  element.scrollTop = element.scrollHeight;
+        catplayer.stop();
+        img.stop();
+        
+        var xpos = parseInt(catplayer.css("left"));
+        var ypos = parseInt(catplayer.css("top"));
+        var xMove = x - xpos;
+        var yMove = y - ypos;
+        var absXMove = Math.abs(xMove);
+        var absYMove = Math.abs(yMove);
+        var xyMove = Math.sqrt(absXMove*absXMove + absYMove*absYMove);
+        var flip = (xMove < 0) ? 1:-1;
+        img.css("transform", "scaleX("+ flip +")");
+        
+        if (xMove >= 0) xMove = "+=" + xMove;
+        else xMove = "-=" + -xMove;
+        
+        if (yMove >= 0) yMove = "+=" + yMove;
+        else yMove = "-=" + -yMove;
+        
+        var realSpeed = (control == "instant") ? 0 : Math.round(xyMove * speed);
+        
+        catplayer.animate({
+                left: xMove,
+                top: yMove
+            }, realSpeed, "linear", function() {
+                checkAction(name, x, y);
+        });
+        
+        img.animate({
+            height: size,
+            width: size
+        }, realSpeed, "linear");
+    }
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var path 	= require('path');
-var app 	= express();
-var http	= require('http').Server(app);
-var io		= require('socket.io')(http);
-var port	= process.env.PORT || 3000;
+var path     = require('path');
+var app     = express();
+var http    = require('http').Server(app);
+var io        = require('socket.io')(http);
+var port    = process.env.PORT || 3000;
 var conn = "postgres://postgres:root@localhost:5432/catclub";
 
 process.setMaxListeners(0);
@@ -11,23 +11,23 @@ process.setMaxListeners(0);
 app.use("/", express.static(__dirname));
 
 var knex = require('knex')({
-  client: 'pg',
-  connection:  process.env.DATABASE_URL || conn,
-  searchPath: 'knex,public'
+    client: 'pg',
+    connection:  process.env.DATABASE_URL || conn,
+    searchPath: 'knex,public'
 });
 
 knex.schema.createTableIfNotExists('user', function (table) {
-  table.increments();
-  table.string('name');
-  table.string('password');
-  table.integer('points').defaultTo(0);
-  table.date("joined");
+    table.increments();
+    table.string('name');
+    table.string('password');
+    table.integer('points').defaultTo(0);
+    table.date("joined");
 }).then();
 
 var bookshelf = require('bookshelf')(knex);
 
 var User = bookshelf.Model.extend({
-  tableName: 'user'
+    tableName: 'user'
 });
 
 var hardcode_users = {};
@@ -36,103 +36,103 @@ var playercount = 0;
 
 io.sockets.on('connection', function(socket){
 
-	var socketid;
-	var name;
-	
-	socket.join("mars");
-	
-	socket.on('playermove', function(x,y,name, control){
-		io.emit('playermove', x, y, name, control);
+    var socketid;
+    var name;
+    
+    socket.join("mars");
+    
+    socket.on('playermove', function(x,y,name, control){
+        io.emit('playermove', x, y, name, control);
     });
-	
-	socket.on('joinroom', function(room, name) {
-		hardcode_users[name] = room;
-		socket.join(room);
-	});
-	
-	socket.on('leaveroom', function(room, name) {
-		io.to(room).emit("playerleftroom", name);
-		socket.leave(room);
-	});
-	
-	socket.on('attemptlogin', function(name, password, register){
-		if (hardcode_users.name != null)
-			socket.emit('errors', 'User already logged in!');
-		else if (!name.match(/^[0-9a-zA-Z]{1,16}$/)){
-			socket.emit('errors', 'Dont cheat cunt!');
-		} else {
-			new User({'name': name})
-			.fetch()
-			.then(function(model) {
-				if (model == null) {
-					if (register)
-						new User({'name': name, 'password': password, 'joined': new Date()}).save().then(function(mdl) {
-							socketid = mdl.get('id');
-							hardcode_users[name] = 'mars';
-							playercount = playercount + 1;
-							socket.emit('login');
-							
-							io.to('mars').emit('chat message', null, name + " joined.", null);
-						});
-					else
-						socket.emit('errors', 'Account doesnt exist!');
-				}
-				else if (!register) {
-					if (model.get('password') == password) {
-						socketid = model.get('id');
-						hardcode_users[name] = 'mars';
-						socket.emit('login');
-						io.to('mars').emit('chat message', null, name + " joined.", null);
-						playercount = playercount + 1;
-					}
-					else {
-						socket.emit('errors', 'Wrong password!');
-					}
-				}
-				else {
-					socket.emit('errors', 'Account already exists!');
-				}
-			});
-		}
+    
+    socket.on('joinroom', function(room, name) {
+        hardcode_users[name] = room;
+        socket.join(room);
     });
-	
-	socket.on('chat message', function(name, msg, control) {
-		
-		
-		if (msg == "!points")
-		{
-			new User({'name': name})
-			.fetch()
-			.then(function(model) {
-				io.to(control).emit('chat message',null,'' + name + ' has ' + model.get('points') + ' points.', null);
-			});
-		}
-		io.to(control).emit('chat message', name, msg, null);
-	});
-	
-	socket.on('reqspawn', function(target, name, x, y) {
-		if (target.startsWith(' ')) {
-			io.to(target.substr(1)).emit('spawn', 'A L L', name, x, y);
-		} else
-			io.emit('spawn', target, name, x, y);
-	});
-	
-	socket.on('disconnect', function() {
-		
-		if (socketid != null)
-			new User({'id': socketid})
-			.fetch()
-			.then(function(model) {
-				var name = model.get('name').replace(/`/g , "");
-				io.emit('chat message', name,'' + name + ' disconnected.', 'disconnect');
-				delete hardcode_users[name];
-				playercount = playercount - 1;
-			});
-		
-	});
+    
+    socket.on('leaveroom', function(room, name) {
+        io.to(room).emit("playerleftroom", name);
+        socket.leave(room);
+    });
+    
+    socket.on('attemptlogin', function(name, password, register){
+        if (hardcode_users.name != null)
+            socket.emit('errors', 'User already logged in!');
+        else if (!name.match(/^[0-9a-zA-Z]{1,16}$/)){
+            socket.emit('errors', 'Dont cheat cunt!');
+        } else {
+            new User({'name': name})
+            .fetch()
+            .then(function(model) {
+                if (model == null) {
+                    if (register)
+                        new User({'name': name, 'password': password, 'joined': new Date()}).save().then(function(mdl) {
+                            socketid = mdl.get('id');
+                            hardcode_users[name] = 'mars';
+                            playercount = playercount + 1;
+                            socket.emit('login');
+                            
+                            io.to('mars').emit('chat message', null, name + " joined.", null);
+                        });
+                    else
+                        socket.emit('errors', 'Account doesnt exist!');
+                }
+                else if (!register) {
+                    if (model.get('password') == password) {
+                        socketid = model.get('id');
+                        hardcode_users[name] = 'mars';
+                        socket.emit('login');
+                        io.to('mars').emit('chat message', null, name + " joined.", null);
+                        playercount = playercount + 1;
+                    }
+                    else {
+                        socket.emit('errors', 'Wrong password!');
+                    }
+                }
+                else {
+                    socket.emit('errors', 'Account already exists!');
+                }
+            });
+        }
+    });
+    
+    socket.on('chat message', function(name, msg, control) {
+        
+        
+        if (msg == "!points")
+        {
+            new User({'name': name})
+            .fetch()
+            .then(function(model) {
+                io.to(control).emit('chat message',null,'' + name + ' has ' + model.get('points') + ' points.', null);
+            });
+        }
+        io.to(control).emit('chat message', name, msg, null);
+    });
+    
+    socket.on('reqspawn', function(target, name, x, y) {
+        if (target.startsWith(' ')) {
+            io.to(target.substr(1)).emit('spawn', 'A L L', name, x, y);
+        } else
+            io.emit('spawn', target, name, x, y);
+    });
+    
+    socket.on('disconnect', function() {
+        
+        if (socketid != null)
+            new User({'id': socketid})
+            .fetch()
+            .then(function(model) {
+                var name = model.get('name').replace(/`/g , "");
+                io.emit('chat message', name,'' + name + ' disconnected.', 'disconnect');
+                delete hardcode_users[name];
+                playercount = playercount - 1;
+            });
+        
+    });
 });
 
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+    console.log('listening on *:' + port);
 });
